e63dd193a36b876c0e7909adb21ca15775795800
Giteak <hgt2768@naver.com>
2022-07-03 16:44:41 +0900

    login.ejs 추가

--- app/routes/index.route.js
+++ app/routes/index.route.js
@@ -1,8 +1,12 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render("index");
-})
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
 module.exports = router;
